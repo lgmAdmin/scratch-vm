a93b4986b4867be7f8447ed3041199e503f8b978
lgm <2663253979@qq.com>
2025-07-06 14:10:08 +0800

    first commit

--- src/extension-support/extension-manager.js
+++ src/extension-support/extension-manager.js
@@ -9,6 +9,8 @@ const SecurityManager = require('./tw-security-manager');
 // TODO: move these out into a separate repository?
 // TODO: change extension spec so that library info, including extension ID, can be collected through static methods
 
+
+//test git branch
 const defaultBuiltinExtensions = {
     // This is an example that isn't loaded with the other core blocks,
     // but serves as a reference for loading core blocks as extensions.
